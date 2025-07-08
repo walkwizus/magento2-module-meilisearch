b5936c8eef199492d818b35fd1e21cd56b1682bb
Jimmy Carricart <jimmy@walkwizus.fr>
2025-07-08 08:16:27 +0200

    fix: Fix searchbar responsive

--- src/module-meilisearch-frontend/view/frontend/web/js/components/autocomplete.js
+++ src/module-meilisearch-frontend/view/frontend/web/js/components/autocomplete.js
@@ -1,16 +1,18 @@
 define([
     'uiComponent',
     'ko',
+    'jquery',
     'Walkwizus_MeilisearchFrontend/js/service/meilisearch-service',
     'Walkwizus_MeilisearchFrontend/js/model/config-model',
     'mage/url'
-], function(Component, ko, meilisearchService, configModel, url) {
+], function(Component, ko, $, meilisearchService, configModel, url) {
     'use strict';
 
     return Component.extend({
         defaults: {
             searchTerm: ko.observable(''),
-            results: ko.observable({})
+            results: ko.observable({}),
+            isActive: ko.observable(false)
         },
 
         initialize: function() {
@@ -24,6 +26,8 @@ define([
             this.searchTerm.subscribe((v) => {
                 this.performSearch(v);
             });
+
+            this._bindClickOutside();
 
             return this;
         },
@@ -41,6 +45,26 @@ define([
 
         getSearchUrl: function() {
             return url.build('catalogsearch/result');
+        },
+
+        toggleSearch: function(el) {
+            this.isActive(!this.isActive());
+            if (this.isActive()) {
+                $('#search').focus();
+            }
+        },
+
+        _bindClickOutside: function() {
+            const self = this;
+            $(document).on('click.miniSearch', (e) => {
+                const $target = $(e.target);
+                const $container = $('.block-search');
+
+                if (!$container.is($target) && $container.has($target).length === 0) {
+                    self.isActive(false);
+                    self.results([]);
+                }
+            });
         }
     });
 });
