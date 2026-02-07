4c23b05d88a38947d10e531ceecb02fb80ae0866
Jimmy Carricart <jimmy@walkwizus.fr>
2026-02-07 13:57:07 +0100

    Save embedder in Meilisearch

--- src/module-meilisearch-ai/Plugin/Controller/IndexSavePlugin.php
+++ src/module-meilisearch-ai/Plugin/Controller/IndexSavePlugin.php
@@ -5,19 +5,26 @@ declare(strict_types=1);
 namespace Walkwizus\MeilisearchAi\Plugin\Controller;
 
 use Magento\Framework\App\RequestInterface;
+use Walkwizus\MeilisearchAi\Api\Data\EmbedderInterface;
 use Walkwizus\MeilisearchAi\Model\ResourceModel\Embedder\Link as LinkResource;
 use Walkwizus\MeilisearchIndices\Controller\Adminhtml\Indices\Save as SaveController;
+use Walkwizus\MeilisearchBase\Service\SettingsManager;
+use Walkwizus\MeilisearchAi\Model\ResourceModel\Embedder\CollectionFactory as EmbedderCollectionFactory;
 
 class IndexSavePlugin
 {
     /**
      * @param LinkResource $linkResource
      * @param RequestInterface $request
+     * @param SettingsManager $settingsManager
+     * @param EmbedderCollectionFactory $embedderCollectionFactory
      */
     public function __construct(
         private readonly LinkResource $linkResource,
-        private readonly RequestInterface $request
-    ) {}
+        private readonly RequestInterface $request,
+        private readonly SettingsManager $settingsManager,
+        private readonly EmbedderCollectionFactory $embedderCollectionFactory
+    ) { }
 
     /**
      * @param SaveController $subject
@@ -26,16 +33,31 @@ class IndexSavePlugin
      */
     public function afterExecute(SaveController $subject, $result)
     {
-        $indexUid = $this->request->getParam('id');
+        $indexUid = (string)$this->request->getParam('id');
         $postData = $this->request->getPostValue();
-
         $embedderIds = $postData['embedder_ids'] ?? [];
 
         if ($indexUid) {
             try {
-                $ids = is_array($embedderIds) ? $embedderIds : explode(',', (string)$embedderIds);
-                $ids = array_filter($ids);
-                $this->linkResource->syncEmbedderLinks((string)$indexUid, $ids);
+                $ids = is_array($embedderIds) ? $embedderIds : array_filter(explode(',', (string)$embedderIds));
+                $this->linkResource->syncEmbedderLinks($indexUid, $ids);
+                $meiliEmbeddersConfig = [];
+                if (!empty($ids)) {
+                    $collection = $this->embedderCollectionFactory->create();
+                    $collection->addFieldToFilter('embedder_id', ['in' => $ids]);
+
+                    /** @var EmbedderInterface $embedder */
+                    foreach ($collection as $embedder) {
+                        $meiliEmbeddersConfig[$embedder->getIdentifier()] = [
+                            'source' => $embedder->getSource(),
+                            'apiKey' => $embedder->getApiKey(),
+                            'model' => $embedder->getModel(),
+                            'documentTemplate' => $embedder->getDocumentTemplate()
+                        ];
+                    }
+                }
+
+                $this->settingsManager->updateEmbedders($indexUid, $meiliEmbeddersConfig);
 
             } catch (\Exception $e) {
 
